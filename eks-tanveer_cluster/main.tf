021a0b8c234ab66798f6a656092749a2b12916a2
tanveerkkhan89 <tanveerkkhan89@gmail.com>
2024-08-10 22:12:09 +0530

    Update main.tf
    change in terraform

--- eks-tanveer_cluster/main.tf
+++ eks-tanveer_cluster/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  backend "s3" {
+    bucket         = "rakbankdemo"
+    key            = "terraform/state.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-lock-table"
+  }
+}
+
 provider "aws" {
   region = "us-east-1"
 }
