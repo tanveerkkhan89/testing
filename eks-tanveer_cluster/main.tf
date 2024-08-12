bca793dc646ed841ad40920bd6fcaf39bd323b2e
tanveerkkhan89 <tanveerkkhan89@gmail.com>
2024-08-12 07:41:45 +0530

    Update main.tf
    change

--- eks-tanveer_cluster/main.tf
+++ eks-tanveer_cluster/main.tf
@@ -11,6 +11,20 @@ provider "aws" {
   region = "us-east-1"
 }
 
+provider "kubernetes" {
+  host                   = "https://${aws_eks_cluster.eks_cluster.endpoint}"
+  token                  = data.aws_eks_cluster_auth.cluster.token
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks_cluster.certificate_authority[0].data)
+}
+
+data "aws_eks_cluster" "eks_cluster" {
+  name = aws_eks_cluster.eks_cluster.name
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = aws_eks_cluster.eks_cluster.name
+}
+
 # VPC
 resource "aws_vpc" "main" {
   cidr_block           = "10.0.0.0/16"
@@ -178,7 +192,6 @@ resource "kubernetes_service" "my_app_lb" {
     port {
       port        = 80        # The port exposed by the LoadBalancer
       target_port = 3000      # The port your application is listening on inside the container
-      node_port   = 30000     # The NodePort that routes traffic to targetPort
     }
   }
 }
